c434ffa9af921ea5f898fc74e80cd5313539b322
tanishaislam <dev.eva.2006@gmail.com>
2024-09-17 20:05:40 +0600

    invoice create and select controller

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Invoice;
+use App\Models\InvoiceProduct;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
@@ -18,5 +21,48 @@ class InvoiceController extends Controller
 
     function InvoiceCreate(Request $request){
         DB::beginTransaction();
+
+        try{
+            $user_id=$request->header('id');
+            $total=$request->input('total');
+            $discount=$request->input('discount');
+            $vat=$request->input('vat');
+            $payable=$request->input('payable');
+            $customer_id=$request->input('customer_id');
+
+            $invoice= Invoice::create([
+                'total'=>$total,
+                'discount'=>$discount,
+                'vat'=>$vat,
+                'payable'=>$payable,
+                'customer_id'=>$customer_id,
+                'user_id'=>$user_id
+            ]);
+
+            $invoiceID=$invoice->id;
+            $products=$request->input('products');
+
+            foreach($products as $EachProduct){
+                InvoiceProduct::create([
+                    'invoice_id'=>$invoiceID,
+                    'user_id'=>$user_id,
+                    'product_id'=>$EachProduct['product_id'],
+                    'qty'=>$EachProduct['qty'],
+                    'sale_price'=>$EachProduct['sale_price']
+                ]);
+            }
+
+            DB::commit();
+            return 1;
+        }
+        catch (Exception $e) {
+            DB::rollBack();
+            return 0;
+        }
+    }
+
+    function InvoiceSelect(Request $request){
+        $user_id=$request->header('id');
+        return Invoice::where('user_id',$user_id)->with('customer')->get();
     }
 }
